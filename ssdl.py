979f2ce5b57c6f13b292f8c8f5e2b18218fde40f
mosm <airman515@gmail.com>
2017-08-25 06:59:23 +0900

    Add comments

--- ssdl.py
+++ ssdl.py
@@ -16,23 +16,31 @@ def download(url):
     doc = bs4.BeautifulSoup(urllib.request.urlopen(url), 'lxml')
     img_urls = list(map(lambda n: n.attrs['data-full'], doc.find_all('img', class_='slide_image')))
     num_imgs = len(img_urls)
+
+    # sort by page number
     img_urls.sort(key=lambda img_url: int(img_url_re.match(img_url).group(1)))
 
     with tempfile.TemporaryDirectory() as dirname:
         img_filenames = []
 
         for i, img_url in tqdm.tqdm(enumerate(img_urls), desc='downloading', total=num_imgs):
+            # no need to include extensions in filenames since ImageMagick determines file types from signatures
             filename = os.path.join(dirname, str(i))
             urllib.request.urlretrieve(img_url, filename)
             img_filenames.append(filename)
 
         print('converting to PDF')
+        # if |url| points at specific page, get title of parent slide
         title_elem = doc.find('a', class_='j-parent-title')
         if title_elem is None:
+            # if no, just get title of slide
             title_elem = doc.find('span', class_='j-title-breadcrumb')
+
+        # ImageMagick uses filename as PDF title
         dest_filename = os.path.join(dirname, '{}.pdf'.format(title_elem.string))
         subprocess.call(['convert'] + img_filenames + [dest_filename])
 
+        # then rename to sanitized title in |url|
         author, title = re.match('https?://www.slideshare.net/([^/]+)/([^/]+)(?:/.+)?', url).groups()
         final_filename = '{}_{}.pdf'.format(author, title)
         os.rename(dest_filename, os.path.join(os.curdir, final_filename))
